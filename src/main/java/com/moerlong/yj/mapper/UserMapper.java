e4379ec9c72dae14c3408ab6db692357296561b5
chengyi3192 <244461001@qq.com>
2019-08-25 00:11:08 +0800

    init

---
+++ src/main/java/com/moerlong/yj/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.moerlong.yj.mapper;
+
+import com.moerlong.yj.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
